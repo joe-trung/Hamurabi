806e9d2d81dd2fc06b5997d8e3cd564e7bcffca7
joe-trung <kieutrungtran@gmail.com>
2023-03-17 16:58:46 -0400

    Add empty functions

---
+++ docs/matuszek/hummurabi.py
@@ -0,0 +1,59 @@
+def play():
+    pass
+
+
+def play_again():
+    pass
+
+
+def sell_land():
+    pass
+
+
+def buy_land():
+    pass
+
+
+def feed_people():
+    pass
+
+
+def acre_to_plant():
+    pass
+
+
+def plague_death():
+    pass
+
+
+def starvation_death():
+    pass
+
+
+
+def uprising():
+    pass
+
+
+def immigration():
+    pass
+
+
+def harvest():
+    pass
+
+
+def grain_eaten_by_rat():
+    pass
+
+
+def new_cost_of_land():
+    pass
+def print_summary():
+    pass
+
+
+def print_final():
+    pass
+
+
